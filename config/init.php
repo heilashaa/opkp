08d87a54da616a131275c5942f09c904182de163
heilashaa <heilash_a_a@mail.ru>
2018-10-21 15:11:58 +0300

    изменен init.php

--- config/init.php
+++ config/init.php
@@ -1,7 +1,6 @@
 <?php
 
-//режим разработки (1) и продакшен (0)
-define("DEBUG", 1);
+define("DEBUG", 1);//режим разработки (1) и продакшен (0)
 define("ROOT", dirname(__DIR__));
 define("WWW", ROOT.'/public');
 define("APP", ROOT.'/app');
